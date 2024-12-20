4e48839078f098b602bb8604736d4c3ac29374a4
Yashvi <yashvi.sharma1204@gmail.com>
2024-12-20 22:44:28 +0530

    modified

--- Basics/FirstProgram.java
+++ Basics/FirstProgram.java
@@ -1,7 +1,7 @@
 package Basics;
-import java.lang.*;
 public class FirstProgram{
     public static void main(String[] args){
-        System.out.println("Welcome!");
+        System.out.println("One");
+        System.out.println("Two");
     }
 }
